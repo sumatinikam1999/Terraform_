01eb0f40d433e8742442ab88657be42ea75f4914
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-01-10 14:31:49 +0000

    updated

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "example" {
 
 resource "aws_key_pair" "devops_key" {
   key_name = "devops"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDG/E5qfnIj9t2NajPb1KhTq5wuFKPH6lz0pGX8nfKkm4tJm7y2rPVKk2gLFwg6I7e99KH8e6SuAy4mIl2+YTUJWGK6pupgodvbjgSe2grvkJ1qiZlmFI+gSK/U2fa/Ycsxp2/XBoKXnYJfXO/tvKYDtBQaqbsK3FhJzKQ4YUZsxWfjPgwwFRy1faflyitOTQz+gn9XOF0lmp9xxg5isYZLV1FD2744k8grKP/alkTM4hln76CxknEhc6APLt7MOtZ1R0qOOYCQThNREgpNgAR0HsrpfAQplbwXOZoogo8jeaq9/qSMls0mBG6C6gIKhLxolGEzOGXOmJwjMNLH57bb"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
