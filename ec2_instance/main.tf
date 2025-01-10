9e745730c1e12171c0d4154c34a9ce645d6fa95b
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-01-10 14:41:16 +0000

    updated gitignore and main

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -5,11 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = var.ami
   instance_type = var.instance_type
-  key_name = aws_key_pair.devops_key.key_name
 }
 
-resource "aws_key_pair" "devops_key" {
-  key_name = "devops"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
 
